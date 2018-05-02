be5e43286376fb06cd80eb8a7995a6f1ea4166ad
Daniel <danielmt10@outlook.com>
2018-05-01 23:01:17 -0500

    Hice unos cambios bien meloHice unos cambios bien meloss

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Data } from '../../services/Data';
+import {AngularFireDatabase} from 'angularfire2/database/database'
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  notas = [];
-  dato ={air:"",earth:"",light:"",tem:""};
-  constructor(public navCtrl: NavController, public data: Data) {
-    this.notas = data.getNotes();
+  notes = [];
+  dates ={air:"",earth:"",light:"",tem:""};
+  constructor(public navCtrl: NavController, public data: Data, public afbs: AngularFireDatabase) {
+    this.notes = data.getNotes();
   }
   public crearData(){
-    this.data.createDato(this.dato);
+    this.afbs.database.ref('plantapp').child('state').set(this.dates);
     this.clearData();
   }
   private clearData(){
-    this.dato.air  = "";
-    this.dato.earth = "";
-    this.dato.light = "";
-    this.dato.tem = "";
+    this.dates.air  = "";
+    this.dates.earth = "";
+    this.dates.light = "";
+    this.dates.tem = "";
   }
 }
