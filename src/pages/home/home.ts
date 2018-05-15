3d6caa4bd99bab2b64a9c9fbe949b6f77ba223ae
andrestein <andresgrandapiedrahita@gmail.com>
2018-05-15 08:56:31 -0500

    añadiendo restricciones

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {AngularFireDatabase} from 'angularfire2/database/database'
+import {AngularFireDatabase} from 'angularfire2/database/database';
+import { ToastController } from 'ionic-angular';
 
 
 @Component({
@@ -11,16 +12,53 @@ export class Inicio {
   dates ={air:"",earth:"",light:"",tem:""};
   icons:Array<{name:String,icon:String}>;
   system={Sl:false, Sr:false, Sa:false};
-  constructor(public navCtrl: NavController, public afdb: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, public afdb: AngularFireDatabase,private toastCtrl: ToastController) {
     this.icons=[{name:'Sistema Lumínico', icon:'bulb'},
                 {name:'Sistema de Riego', icon:'color-fill'},
                 {name:'Sistema de Acceso', icon:'key'}];
 
   }
-  public crearData(){
-    this.afdb.database.ref('plantapp').child('state').set(this.dates);
-    this.clearData();
+  public crearData(){    
+    if(this.validarCampoNulo()){
+      console.log(Number.isNaN(+this.dates.air));
+      if(this.validarNumero()){
+        this.afdb.database.ref('plantapp').child('state').set(this.dates);
+        this.clearData();
+        this.presentToast('los datos se cargaron con exito');
+      }else{
+        this.presentToast('Los datos deben ser de tipo numerico');  
+      }
+    }else{
+      this.presentToast('No deben existir campos vacios');
+    }
   }
+
+  private presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'middle'
+    });  
+    toast.present();
+  }
+
+  private validarCampoNulo():boolean{
+    if(this.dates.air !="" && this.dates.earth !="" && this.dates.light !="" && this.dates.tem !=""){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  private validarNumero():boolean{
+    if(Number.isNaN(+this.dates.air) || Number.isNaN(+this.dates.air)
+    || Number.isNaN(+this.dates.air) || Number.isNaN(+this.dates.air)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
   private clearData(){
     this.dates.air  = "";
     this.dates.earth = "";
